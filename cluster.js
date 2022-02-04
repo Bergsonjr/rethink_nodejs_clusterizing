25e362d6de844bd33f27b2d81fa3a7006ca8d531
bergsonjr <bergsonjr@icloud.com>
2022-02-04 11:47:41 -0300

    code style config

--- cluster.js
+++ cluster.js
@@ -3,22 +3,22 @@ const numCPUs = require('os').cpus().length;
 const server = require('./server');
 
 (async () => {
-    if (cluster.isMaster) {
-        for (let i = 0; i < numCPUs; i++) {
-            cluster.fork();
-        }
+	if (cluster.isMaster) {
+		for (let i = 0; i < numCPUs; i++) {
+			cluster.fork();
+		}
 
-        cluster.on('exit', (worker, code, signal) => {
-            console.info(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
+		cluster.on('exit', (worker, code, signal) => {
+			console.info(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
 
-            cluster.fork();
-            console.info('Starting a new worker');
-        });
-    } else {
-        await server.start();
+			cluster.fork();
+			console.info('Starting a new worker');
+		});
+	} else {
+		await server.start();
 
-        console.info('Master process is running');
-    }
+		console.info('Master process is running');
+	}
 })();
 
 //252793
--- server.js
+++ server.js
@@ -7,37 +7,37 @@ const getRandomIntInclusive = require('./src/utils/getRandomIntInclusive');
 const factorial = require('./src/utils/factorial');
 
 const fastify = require('fastify')({
-    logger: true,
-    disableRequestLogging: true,
+	logger: true,
+	disableRequestLogging: true,
 });
 
 fastify.get('/', async (request, reply) => {
-    return { hello: 'world' };
+	return { hello: 'world' };
 });
 
 // Rota para gerar maior processamento/consumo CPU
 fastify.get('/fatorial', async (request, reply) => {
-    return factorial(getRandomIntInclusive(10, 25));
+	return factorial(getRandomIntInclusive(10, 25));
 });
 
 // Rota para "quebrar" o processo
 fastify.get('/fail', async (request, reply) => {
-    readFileAsync('arquivo_inexistente.txt');
+	readFileAsync('arquivo_inexistente.txt');
 });
 
 const start = async () => {
-    try {
-        await fastify.listen(process.env.PORT);
-        console.info(`server listening on ${fastify.server.address().port} in process ${process.pid}`);
-    } catch (err) {
-        console.error('error while trying to server up');
-        process.exit(1);
-    }
+	try {
+		await fastify.listen(process.env.PORT);
+		console.info(`server listening on ${fastify.server.address().port} in process ${process.pid}`);
+	} catch (err) {
+		console.error('error while trying to server up');
+		process.exit(1);
+	}
 };
 
 process.on('unhandledRejection', function (err) {
-    console.log(`unhandledRejection: killing process ${process.pid}`);
-    process.exit(1);
+	console.log(`unhandledRejection: killing process ${process.pid}`);
+	process.exit(1);
 });
 
 module.exports = { start };
