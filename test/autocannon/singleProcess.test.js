e4e33c96986b327f5da320c0fdb7f649a78cb23c
bergsonjr <bergsonjr@icloud.com>
2022-02-10 12:45:31 -0300

    fix test config

--- test/autocannon/singleProcess.test.js
+++ test/autocannon/singleProcess.test.js
@@ -1,17 +1,16 @@
 const autocannon = require('autocannon');
 
 function testSingleProcess() {
-    const instance = autocannon({
-        title: 'Test for Node.js single process',
-        url: 'http://localhost:3000/fatorial?n=10',
-        method: 'GET',
-        connections: 100, // número de conexões concorrentes
-        pipelining: 10, // número de solicitações HTTP simultâneas por conexão TCP
-        duration: 120, // tempo de execução do teste
-    });
+	const instance = autocannon({
+		title: 'Test for Node.js single process',
+		url: 'http://localhost:3000/fatorial?n=10',
+		method: 'GET',
+		connections: 10, // número de conexões concorrentes
+		pipelining: 1, // número de solicitações HTTP simultâneas por conexão TCP
+		duration: 30, // tempo de execução do teste
+	});
 
-    autocannon.track(instance, { renderProgressBar: true, renderResultsTable: true, renderLatencyTable: true });
+	autocannon.track(instance, { renderProgressBar: true, renderResultsTable: true, renderLatencyTable: true });
 }
 
 testSingleProcess();
-
